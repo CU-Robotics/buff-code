5b84ba6717e169bad80b05d57257d2d1e439588e
Arjun Lakshmi Narasimhan <larjun01@rgnt1-59-174-dhcp.int.colorado.edu>
2021-10-01 19:00:18 -0600

    Updated install_macos.bash

--- scripts/install_macos.bash
+++ scripts/install_macos.bash
@@ -11,13 +11,9 @@ brew update
 
 echo -e "\n\tInstalling Dependencies...\n"
 
-brew install python3
-python3 -m pip install --upgrade pip
-brew install arduino-cli
-pip install testresources
+xargs brew install <${PROJECT_ROOT}/config/dependencies.txt 
 
 pip3 install -r ${PROJECT_ROOT}/config/python3_requirements.txt
 
 export PATH="${PROJECT_ROOT}/buffpy:${PROJECT_ROOT}/buffpy/arduino-cli_0.19.2_Linux_64bit:${PATH}"
 export PYTHONPATH="${PROJECT_ROOT}/buffpy/bin:${PYTHONPATH}"
-arduino-cli core install arduino:samd
