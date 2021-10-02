fa1922d8edf886df7b82f7e72c33d7873744acbd
Mitchell D Scott <misc4432@colorado.edu>
2021-10-01 22:35:35 -0600

    init

--- buffpy/setup.py
+++ buffpy/setup.py
@@ -1,11 +1,3 @@
 #! /usr/bin/env python3
 
-from disutils.core import setup
-
-setup(
-	name='BuffPy',
-	version='0.0'
-	description='CU-Robotics workspace manager',
-	author='Mitchell D Scott',
-	author_email='mitchell.scott@colorado.edu',
-	packages=['buffpy'])
+# a setup file for when buffpy becomes a true python package
