302ef6c0cab8e07f467522230dc1bfab0097bef6
Mitchell D Scott <misc4432@colorado.edu>
2021-10-05 17:51:23 -0600

    readme update

--- scripts/install.bash
+++ scripts/install.bash
@@ -7,13 +7,17 @@ DIR=$PWD
 echo -e "Running Install from ${PWD}"
 echo -e "\n\tapt updating...\n"
 
+#	update the apt package manager
 sudo apt update
 
 echo -e "\n\tInstalling Dependencies...\n"
 
+#	Using apt and pip install all the dependencies for the project
 xargs sudo apt install <${PROJECT_ROOT}/config/dependencies.txt -y
 pip3 install -r ${PROJECT_ROOT}/config/python3_requirements.txt
 
+#	Install arduino-cli
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=${DIR}/buffpy/bin sh
 
+#	Install SamD boards (subject to change)
 arduino-cli core install arduino:samd
