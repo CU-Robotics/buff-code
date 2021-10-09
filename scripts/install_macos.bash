6ca3f8bdebfdb6700e683e77483b02a1fd8ca5dd
Arjun Lakshmi Narasimhan <larjun01@rgnt1-58-243-dhcp.int.colorado.edu>
2021-10-08 18:55:55 -0600

    Updated install with config/install

--- scripts/install_macos.bash
+++ scripts/install_macos.bash
@@ -12,10 +12,17 @@ brew upgrade
 
 echo -e "\n\tInstalling Dependencies...\n"
 
+<<<<<<< HEAD
 xargs brew install <${PWD}/config/install/dependencies.txt 
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=${DIR}/buffpy/bin sh
 
 pip3 install -r ${PWD}/config/install/python3_requirements.txt
+=======
+xargs brew install <${PROJECT_ROOT}/config/install/dependencies.txt 
+brew install arduino-cli
 
-export PATH="${PROJECT_ROOT}/buffpy:${PROJECT_ROOT}/buffpy/arduino-cli"
-export PYTHONPATH="${PROJECT_ROOT}/buffpy/bin:${PYTHONPATH}"
+pip3 install -r ${PROJECT_ROOT}/config/install/python3_requirements.txt
+>>>>>>> cf627da52f077c0b7d2535e30334532294b8415c
+
+export PATH="${PROJECT_ROOT}/buffpy/bin:${PATH}"
+export PYTHONPATH="${PROJECT_ROOT}/buffpy/lib:${PYTHONPATH}"
