bd34529e5aa54a9871bf392230e9bcb8898faa20
Mitchell D Scott <misc4432@colorado.edu>
2021-10-12 16:22:47 -0600

    late push

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-#  run this script with 'source install.bash'
+#  run this script with 'source scripts/install.bash'
 
 DIR=$PWD
 
@@ -13,11 +13,11 @@ sudo apt update
 echo -e "\n\tInstalling Dependencies...\n"
 
 #	Using apt and pip install all the dependencies for the project
-xargs sudo apt install <${PROJECT_ROOT}/config/dependencies.txt -y
-pip3 install -r ${PROJECT_ROOT}/config/python3_requirements.txt
+xargs sudo apt install <${PROJECT_ROOT}/config/install/dependencies.txt -y
+pip3 install -r ${PROJECT_ROOT}/config/install/python3_requirements.txt
 
 #	Install arduino-cli
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=${DIR}/buffpy/bin sh
 
-#	Install SamD boards (subject to change)
+#	Install SamD boards (subject to change, need to know the teensy's fqbn)
 arduino-cli core install arduino:samd
