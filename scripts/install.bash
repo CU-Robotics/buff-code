6075c6d96bd9f3c01c436b06e24dc4679d0ca300
Mitchell D Scott <misc4432@colorado.edu>
2021-10-12 23:12:23 -0600

    teensy install added

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,7 +2,7 @@
 
 #  run this script with 'source scripts/install.bash'
 
-DIR=$PWD
+PROJECT_ROOT=$PWD
 
 echo -e "Running Install from ${PWD}"
 echo -e "\n\tapt updating...\n"
@@ -16,8 +16,24 @@ echo -e "\n\tInstalling Dependencies...\n"
 xargs sudo apt install <${PROJECT_ROOT}/config/install/dependencies.txt -y
 pip3 install -r ${PROJECT_ROOT}/config/install/python3_requirements.txt
 
+# DEPRECATED
 #	Install arduino-cli
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=${DIR}/buffpy/bin sh
-
+#	Switching to teensy loader
+#curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=${DIR}/buffpy/bin sh
 #	Install SamD boards (subject to change, need to know the teensy's fqbn)
-arduino-cli core install arduino:samd
+# arduino-cli core install arduino:samd
+
+#	Pull teensy files
+# teensy binary and objects
+curl https://www.pjrc.com/teensy/teensy_linux64.tar.gz -O
+# teensy rules file
+curl https://www.pjrc.com/teensy/00-teensy.rules -O
+
+# mv rules into rules.d and set the proper file permissions
+sudo mv 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+sudo chmod 0644 /etc/udev/rules.d/00-teensy.rules
+
+# extract the tar to buffpy/bin
+tar -xvsf teensy_linux.tar.gz -C ${PROJECT_ROOT}/buffpy/bin
+# remove unecessary tar.gz 
+rm teensy_linux.tar.gz
