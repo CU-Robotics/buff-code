17dadfb9247b49de22469afc0760a7dce34e18ff
Mitchell D Scott <misc4432@colorado.edu>
2021-10-22 21:48:20 -0600

    ros install updates

--- build/buff_ros/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "buff_ros"
-PROJECT_SPACE_DIR = "/home/m-dyse/buff-code/devel/.private/buff_ros"
-PROJECT_VERSION = "0.0.0"
