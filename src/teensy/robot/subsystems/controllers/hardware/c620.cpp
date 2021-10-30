848a2bdf0d7157c78b3681b1ab11d84416a4433a
Tyler Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2021-10-29 18:44:19 -0600

    more c620

--- src/teensy/robot/subsystems/controllers/hardware/c620.cpp
+++ src/teensy/robot/subsystems/controllers/hardware/c620.cpp
@@ -3,8 +3,16 @@
 c620::rmMotor(short tempID, CAN_message_t &msg) {
     id = tempID;
     &sendMsg = msg;
+    byteNum = id - 1;
+    if(byteNum > 3) {
+        byteNum -= 4;
+    }
 }
 
-c620::set(float power) {
-    
+c620::setPower(float power) {
+    short newPower = (short)(power * C620_MAX_VALUE);
+    byte byteOne = highByte(newPower);
+    byte byteTwo = lowByte(newPower);
+    sendMsg.buf(byteNum) = highByte;
+    sendMsg.buf(byteNum + 1) = lowByte;
 }
