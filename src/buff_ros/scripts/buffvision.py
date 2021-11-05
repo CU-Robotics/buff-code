480306753cd03eb22cc96fc9df5bbb02ee3e0a93
Mitchell D Scott <misc4432@colorado.edu>
2021-11-05 17:21:09 -0600

    stashing

--- src/buff_ros/scripts/buffvision.py
+++ src/buff_ros/scripts/buffvision.py
@@ -20,7 +20,9 @@ import rospy
 import datetime
 import traceback
 import numpy as np
+from cv_bridge import CvBridge
 import matplotlib.pyplot as plt
+from sensor_msgs.msg import Image
 import xml.etree.ElementTree as ET
 from gdrive_handler import GD_Handler
 
@@ -87,17 +89,39 @@ def display_annotated(data):
 		
 	buffshow('annotated', image)
 
-def datefilledPath(type, ext='.png'):
-	return f'/home/cu-robotics/buffvision/{type}/{datetime.datetime.now()}{ext}'
+def dateFilledPath(ext='.png'):
+	"""
+		create a date and time stamped filepath. Root is always PROJECT_ROOT/data
+		@PARAMS
+			ext: the extension of the file, must match the file type (not checked)
+		@RETURNS
+			date filled filepath
+	"""
+	return os.path.join(os.getenv('PROJECT_ROOT'), 'data', datetime.datetime.now() + ext)
 
 def save_image(image, filepath=None):
-
+	"""
+		Use cv2 to save an image.
+		@PARAMS
+			image: the image to save (np array, cv2.mat... etc)
+			filepath: place to save the image, if None will become a date
+				timestamp and be saved in data (if it exists).
+		@RETURNS
+			None
+	"""
 	if not filepath is None:
 		cv2.imwrite(filepath, image)
 	else:
-		cv2.imwrite(datefilledPath('data'), image)
+		cv2.imwrite(dateFilledPath(), image)
 
 def single_image_capture_cv():
+	"""
+		Use cv2 to create a stream to a camera and capture a single image.
+		@PARAMS
+			None
+		@RETURNS
+			None
+	"""
 	camera = cv2.VideoCapture(0)
 
 	if camera.isOpened():
@@ -107,7 +131,13 @@ def single_image_capture_cv():
 	camera.release()
 
 def capture_video(duration=10):
-	
+	"""
+		Use cv2 to create a stream to a camera and record a video.
+		@PARAMS
+			duration: length in seconds of the video (not exact)
+		@RETURNS
+			None
+	"""
 	ext = 'avi'
 	cap = cv2.VideoCapture(0)
 	size=(int(cap.get(3)), int(cap.get(4)))
@@ -125,7 +155,52 @@ def capture_video(duration=10):
 	cap.release()
 	out.release()
 
+def ROS_cv2_publisher(topic='image_raw'):
+	"""
+		Use cv2 to create a stream to a camera and publish the images
+		to a ROS topic.
+		@PARAMS
+			topic: the name of the topic the image will publish to
+		@RETURNS
+			None
+	"""
+
+	# Create the image stream
+	cap = cv2.VideoCapture(0)
+	# Set a size variable (resolution)
+	size=(int(cap.get(3)), int(cap.get(4)))
+
+	# Create the image publisher
+	pub = rospy.Publisher(topic, Image, queue_size=1)
+	# Init this program as a ROS node
+	# anonymous sets a unique node ID
+	rospy.init_node('image_streamer', anonymous=True)
+
+	# Initialize the bridge object
+	# CvBridge is used to conver cv images to sensor_msgs/Image
+	bridge = CvBridge()
+
+	# If the stream is open and ROS is running
+	if cap.isOpened():
+		while not rospy.is_shutdown():
+
+			# Capture frame
+			ret, frame = cap.read()
+
+			# Convert the cv frame to a ROS message
+			imgMsg = bridge.cv2_to_imgmsg(frame, "bgr8")
+
+			# Publish the message
+			pub.publish(imgMsg)
+
+
+
+
 
 if __name__=='__main__':
-	single_image_capture_cv()
+	#single_image_capture_cv()
 	#capture_video()
+
+	ROS_cv2_publisher()
+	
+		
