5f1e1dffec4c73b0eac016f548285ac296ab1977
Mitchell D Scott <misc4432@colorado.edu>
2021-11-19 17:13:34 -0700

    roslaunch

--- src/buff_ros/scripts/mds_detector.py
+++ src/buff_ros/scripts/mds_detector.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 """
 	Project:
 			Mitch detector
@@ -7,35 +7,63 @@
 		Detects and displays images
 """
 import os
+import sys
 import cv2
 import yaml
 import rospy
 import numpy as np
-import buffvision as bv
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray
 
 class MDS_Detector:
-	def __init__(self, low, high, color=(0, 255, 0), thickness=2, in_topic='image_raw', out_topic='detected_boundary', with_core=False):
+	def __init__(self, config=None, low=None, high=None, 
+					color=(0, 255, 0), thickness=2, 
+					in_topic='image_raw', out_topic='detected_boundary', 
+					with_core=False, debug=False):
 		"""
 			Define all the parameters of the model here.
 			No need to save images or results
 		"""
-		self.low = low #  Lower boundary of threshold
-		self.high = high #  Upper boundary of threshold
+		if config is None:
+			if low is None or high is None:
+				# exit if no bounds or config
+				return None
+			self.low = low #  Lower boundary of threshold
+			self.high = high #  Upper boundary of threshold
+		else:
+			# read config params
+			file = os.path.join(os.getenv('PROJECT_ROOT'), 'config', 'lib', '{}.yaml'.format(config))
+			with open(file, 'r') as f:
+				bounds = yaml.safe_load(f)
+				self.low = np.array(bounds['LOW'])
+				self.high = np.array(bounds['HIGH'])
+				if 'INTOPIC' in bounds:
+					in_topic = bounds['INTOPIC']
+				if 'OUTOPIC' in bounds:
+					out_topic = bounds['OUTTOPIC']
+				if 'CORE' in bounds:
+					with_core = bounds['CORE']
+				if 'DEBUG' in bounds:
+					debug = bounds['DEBUG']
 
 		self.steps = None
-
-		self.ANNOTATION_COLOR = color 
+		self.ANNOTATION_COLOR = color
 		self.ANNOTATION_THICKNESS = thickness
 
 		# Only spin up image sub if core is running
 		if with_core:
+
+			self.debug = debug
 			self.in_topic = in_topic
 			self.out_topic = out_topic
+
+			self.bridge = CvBridge()
 
 			rospy.init_node('mds_detector', anonymous=True)
 
 			self.bound_pub = rospy.Publisher(out_topic, Float64MultiArray, queue_size=1)
+			self.annotate_pub = rospy.Publisher('annotated_image', Image, queue_size=1)
 			self.im_subscriber = rospy.Subscriber(in_topic, Image, self.imageCallBack, queue_size=1)
 
 	def drawLines(self, image, contour):
@@ -70,7 +98,7 @@ class MDS_Detector:
 		mask = cv2.cvtColor(thresh, cv2.COLOR_RGB2GRAY)
 		# if you don't know this already,
 		# you need to go read the docs
-		contours, hierarchy = cv2.findContours(image=mask, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)	
+		_, contours, _ = cv2.findContours(image=mask, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)	
 
 		if steps:
 			annotated = image
@@ -97,7 +125,7 @@ class MDS_Detector:
 		bv.buffshow('Steps', self.steps)
 
 
-	def detect_and_publish(self, image):
+	def detect_and_publish(self, image, steps=False):
 		"""
 			Define all image processing operations here.
 			Publishes the bounds to topic instead of returning.
@@ -107,7 +135,7 @@ class MDS_Detector:
 			@RETURNS:
 				None
 		"""
-		results = self.detect(image)
+		results = self.detect(image, steps=steps)
 		mesg = Float64MultiArray()
 		mesg.data = results
 		self.bound_pub.publish(mesg)
@@ -120,31 +148,18 @@ class MDS_Detector:
 			@RETURNS:
 				None
 		"""
-		detect_and_publish(img_msg.data) # Not tested
 		
+		if self.debug:
+			self.detect_and_publish(self.bridge.imgmsg_to_cv2(img_msg), True) # Not tested
+			self.annotate_pub.publish(self.bridge.cv2_to_imgmsg(self.steps, encoding='bgr8'))
+
+		else:
+			self.detect_and_publish(self.bridge.imgmsg_to_cv2(img_msg))
 
 def main():
-	idx = [20, 21, 22, 23] # for looking at specific images
-	data_path = os.path.join(os.getenv('PROJECT_ROOT'), 'data')
-	data = bv.load_data(path=data_path)
-	red_hsv_low = (120,108,180)  # hsv for red, lower boundary
-	red_hsv_high = (128,145,240) # hsv for red, upper boundary
-	blue_hsv_low = (0,45,165) # hsv for blue, lower boundary tuned on cold data
-	blue_hsv_high = (175,100,250) # hsv for blue, upper boundary tuned on cold data
+	detector = MDS_Detector(config=sys.argv[1])
 
-	# Replace with Yaml.read ^^
-
-	blue_det = MDS_Detector(blue_hsv_low, blue_hsv_high, with_core=True)
-
-	red_det = MDS_Detector(red_hsv_low, red_hsv_high)
-
-	for i in idx:
-		image, label = data[i]
-
-	# for image, label in data:
-
-		red_det.detect_and_annotate(image.copy())
-		blue_det.detect_and_annotate(image)
+	rospy.spin()
 
 if __name__=='__main__':
 	main()
