595311586925dac0001a51ce746c491c9205b417
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-01-13 08:59:06 -0700

    removing files in preparation for merging master

--- src/teensy/robot/constants.h
+++
@@ -1,7 +0,0 @@
-//This is a file to store all constants for the whole robot, this includes conversion factors as well as constants for things like PID controllers
-#define CONSTANTS_H //used to check if this file is already included
-
-
-//max values
-#define C620_MAX_VALUE 16384
-#define GM6020_MAX_VALUE 30000
