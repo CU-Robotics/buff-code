54a9b0cdd2a062533a705b9a12cbb089e1f1565b
Tyler-Scripps <tylerscripps@gmail.com>
2022-01-19 12:49:51 -0700

    basically just changed frequency from 500khz to 500hz

--- src/wrecker/src/example/pwm_test/pwm_test.ino
+++ src/wrecker/src/example/pwm_test/pwm_test.ino
@@ -2,14 +2,15 @@ int newValue = 0;
 
 void setup() {
   Serial.begin(115200);
-  analogWriteFrequency(4, 500000);
-  analogWrite(4,128);
+  analogWriteFrequency(4, 500);
+  analogWriteResolution(15);
+  analogWrite(4,256);
 }
 
 void loop() {
   if(Serial.available() > 1) {
     newValue = Serial.parseInt();
-    if(newValue >= 0 && newValue <= 256) {
+    if(newValue >= 0) {
       analogWrite(4, newValue);
       Serial.print("Success: ");
       Serial.println(newValue);
