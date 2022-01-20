f040fdb3f9bf043befc1065ce64405303c90523b
Tyler-Scripps <tylerscripps@gmail.com>
2022-01-20 13:29:40 -0700

    started c620PWM class

--- src/wrecker/src/robot/c620.cpp
+++ src/wrecker/src/robot/c620.cpp
@@ -28,3 +28,21 @@ void c620CAN::setPower(float power) {
     sendMsg.buf[byteNum] = byteOne;
     sendMsg.buf[byteNum + 1] = byteTwo;
 }
+
+c620PWM::c620PWM(uint8_t input, uint8_t output) {
+  //On the Teensy 4.1 input can be pin 0-9,22-25,28,29,33,36,37,42-47, 48-50(dups),51, 52-53 (dups), 54
+  if ((input >= 0 && input <= 9) || (input >= 22 && <= 25) || input == 28 || input == 29 || input == 33 || input == 36 || input == 37 || (input >= 42 && input <= 54))
+  {
+    inPin = input;
+  } else {
+    inPin = -1;
+  }
+  
+  if ((output >= 0 && output <= 15) || output == 18 || output == 19 || (output >= 22 && output <= 25) || output == 28 || output == 29 || output == 33 || output == 36 || output == 37 || (output >= 42 && output <= 47) || output == 51 || output == 54)
+  {
+    outPin = output;
+  } else {
+    outPin = -1;
+  }
+  
+}
