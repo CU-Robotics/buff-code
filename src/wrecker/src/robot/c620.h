f113ebe6bbb18f5ab206021098384b58bd4d306d
Tyler-Scripps <tylerscripps@gmail.com>
2022-01-20 12:56:49 -0700

    changed names to be CAN specific

--- src/wrecker/src/robot/c620.h
+++ src/wrecker/src/robot/c620.h
@@ -10,12 +10,12 @@
 #include <FlexCAN_T4.h>
 #endif
 
-class c620 : public rmMotor {
+class c620CAN : public rmMotor {
     private:
         int id;
         CAN_message_t& sendMsg;
         int byteNum;
     public:
-        c620(int tempID, CAN_message_t* msg);
+        c620CAN(int tempID, CAN_message_t* msg);
         void setPower(float power);
 };
