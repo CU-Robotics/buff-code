99d3f8294b3128f58d50cb2887fa6353c03ade43
Arjun Lakshmi Narasimhan <larjun01@engr2-1-29-dhcp.int.colorado.edu>
2022-02-01 15:26:41 -0700

    Created pid logic

---
+++ src/wrecker/src/robot/pid.h
@@ -0,0 +1,22 @@
+#ifndef _PID_H_
+#define _PID_H_
+
+class PID {
+    private:
+        double _dt; //Change in time interval
+        double _min; // Min output
+        double _max; // Max output
+        double _Kp; // Proportional gain
+        double _Ki; // Integral gain
+        double _Kd; // Derivative gain
+        double _initError; 
+        double _integral;
+
+    public:
+        PID(double dt, double min, double max, double Kp, double Ki, double Kd);
+        double calculate(double setpoint, double pv);
+        ~PID();
+
+};
+
+#endif
