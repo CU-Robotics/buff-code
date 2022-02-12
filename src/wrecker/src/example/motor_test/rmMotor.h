9545d696d75a57b9c783721ab605e90b3923f916
James Bohn <jbohn3353@gmail.com>
2022-02-11 19:48:20 -0700

    Fixed c620 and rmMotor, made tests that pass

---
+++ src/wrecker/src/example/motor_test/rmMotor.h
@@ -0,0 +1,24 @@
+#define RMMOTOR_H
+
+#ifndef CONSTANTS_H
+#include "constants.h"
+#endif
+
+class rmMotor {
+  public:
+    short getTorque();
+    short getRpm();
+    short getAngle();
+    byte getTemp();
+    void updateMotor(CAN_message_t* recMsg);     //update motor info values directly from CAN message  !!!!!!needs implementation
+    void updateMotor(short newTorque, short newRpm, short newAngle, byte newTemp);  //update motor info values by passing in values (that have been extracted from CAN message, for example)
+  private:    //may need to be protected instaed of private if subclasses need to access values
+    short torque = -1;
+    short rpm = -1;
+    short angle = -1;
+    byte temp = -1;
+  protected:
+    short id = -1;
+    short byteNum = -1;
+    CAN_message_t *sendMsgPtr = NULL;
+};
