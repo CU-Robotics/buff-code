2a23bf411ab37a766d6c46faf6a3596301f3b111
Tyler-Scripps <tylerscripps@gmail.com>
2022-03-04 20:23:31 -0700

    pwm encoder and can bus stuff

---
+++ src/wrecker/src/example/swerve_steer_test/swerve_steer_test.ino
@@ -0,0 +1,80 @@
+#include "c620.h" // change to whatever the motor controller is
+
+CAN_message_t sendMsg;
+CAN_message_t recMsg;
+
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
+c610Enc controller1 = c610Enc(1, &sendMsg, 6);
+
+int del = 1; // delay length, figure out why this is necesary!!!!!!!!!
+
+const bool printInfo = true;   //set to true to get stats of motors printed to serial
+
+float power;
+
+float currentAngle = 0;
+float targetAngle = 0;
+float error = 0;
+float dError = 0;
+float tempError = 0;
+
+float kp = (1/10);
+float kd = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  can1.begin();
+  can1.setBaudRate(1000000);
+  Serial.begin(57600);
+
+  Serial.println("Hello from teensy");
+}
+
+void loop() {
+
+  // controller1.setPower(.1);
+
+  currentAngle = controller1.getAngle();
+  error = ((targetAngle - currentAngle) / 360);
+  dError = error - tempError;
+  tempError = error;
+  power = (error * kp) - (dError * kd);
+  controller1.setPower(power);
+  can1.write(sendMsg);
+    
+  delay(del);
+  if(printInfo && can1.read(recMsg)) {
+
+    controller1.updateMotor(&recMsg);
+    
+    Serial.println("------------------");
+    Serial.print("motor id: ");
+    Serial.println(recMsg.id, HEX);
+    Serial.print("Motor angle: ");
+    Serial.println(controller1.getAngle());
+    Serial.print("Torque: ");
+    Serial.println(controller1.getTorque());
+    Serial.print("RPM: ");
+    Serial.println(controller1.getRpm());
+    Serial.print("Error: ");
+    Serial.println(error);
+    Serial.print("dError: ");
+    Serial.println(dError);
+    Serial.print("Power: ");
+    Serial.println(power);
+    
+  }
+
+  if(Serial.available() > 1) {
+    String data = Serial.read();
+    targetAngle = Serial.parseFloat();
+    // currentAngle = controller1.getAngle();
+    // error = targetAngle - currentAngle;
+    // controller1.setPower(error / (360 * 10));
+    // can1.write(sendMsg);
+    Serial.print("new target: ");
+    Serial.println(targetAngle);
+  }
+  
+}
