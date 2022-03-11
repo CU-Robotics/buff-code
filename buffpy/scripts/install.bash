a558cb9f8bfccd7f5ee6290a01d1ad5560d375df
Mitchell D Scott <scott.mitchell913@gmail.com>
2022-03-11 15:24:31 -0700

    install

--- buffpy/scripts/install.bash
+++ buffpy/scripts/install.bash
@@ -2,8 +2,8 @@
 
 #  run this script with 'source scripts/install.bash'
 
-ROSDISTRO=melodic
-ROSVERSION=desktop
+ROS_PKG=desktop
+ROS_DISTRO=melodic
 export PROJECT_ROOT=$PWD
 
 echo -e "Running Install from ${PWD}"
@@ -18,46 +18,45 @@ echo -e "\n\tapt updating...\n"
 sudo apt update
 
 
-echo -e "\n\tInstalling Dependencies...\n"
+echo -e "\n\tapt Installing from ${PROJECT_ROOT}/buffpy/config/install/dependencies.txt\n"
 #	Using apt and pip install all the dependencies for the project
 xargs sudo apt install -y <${PROJECT_ROOT}/buffpy/config/install/dependencies.txt
 sudo rm -rf /var/lib/apt/lists/*
 
-echo -e "\n\tUpgrading pip3\n"
+echo -e "\n\tUpgrading pip3 to 21.3.1\n"
 # Not sure whats happening apt only installs pip==9.0.1
 sudo apt purge -y python3-pip
 # upgrade pip before installing dependencies
-python3 -m pip install --upgrade pip==21.3.1 setuptools wheel
+python3 -m pip install --upgrade pip==21.3.1
 
-export MPLLOCALFREETYPE=1
-echo -e "\n\tInstalling python3 requirements\n"
+echo -e "\n\tpip3 Installing from ${PROJECT_ROOT}/buffpy/config/install/python3_requirements.txt\n"
 python3 -m pip install -r ${PROJECT_ROOT}/buffpy/config/install/python3_requirements.txt
 
 # If no ROS, install it
-if [[ $ROS_DISTRO == "" ]]; then
-	source "${PROJECT_ROOT}/buffpy/scripts/install_ros.bash" $ROSDISTRO $ROSVERSION
+if [[ ! -d /opt/ros/${ROS_DISTRO} ]]; then
+	source "${PROJECT_ROOT}/buffpy/scripts/install_ros.bash" $ROS_DISTRO $ROS_PKG
 fi
 
 # Also install Sublime Text-editor
-# source "${PROJECT_ROOT}/buffpy/scripts/install_sublime.bash"
+## source "${PROJECT_ROOT}/buffpy/scripts/install_sublime.bash"
 
 if [[ "${HOSTNAME}" != "edge"* ]]; then
-	echo -e "\n\tInstalling Teensy loader...\n"
+	# echo -e "\n\tInstalling Teensy loader...\n"
 
-	#	Pull teensy files from pjrc.com
-	# teensy binary and objects
-	curl https://www.pjrc.com/teensy/teensy_linux64.tar.gz -O
-	# teensy rules file
-	curl https://www.pjrc.com/teensy/00-teensy.rules -O
+	# #	Pull teensy files from pjrc.com
+	# # teensy binary and objects
+	# curl https://www.pjrc.com/teensy/teensy_linux64.tar.gz -O
+	# # teensy rules file
+	# curl https://www.pjrc.com/teensy/00-teensy.rules -O
 
-	# mv rules into rules.d and set the proper file permissions
-	sudo mv 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
-	sudo chmod 0644 /etc/udev/rules.d/00-teensy.rules
+	# # mv rules into rules.d and set the proper file permissions
+	# sudo mv 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+	# sudo chmod 0644 /etc/udev/rules.d/00-teensy.rules
 
-	# extract the tar to buffpy/bin
-	tar -xvsf teensy_linux64.tar.gz -C ${PROJECT_ROOT}/buffpy/bin
-	# remove unecessary tar.gz 
-	rm teensy_linux64.tar.gz
+	# # extract the tar to buffpy/bin
+	# tar -xvsf teensy_linux64.tar.gz -C ${PROJECT_ROOT}/buffpy/bin
+	# # remove unecessary tar.gz 
+	# rm teensy_linux64.tar.gz
 
 else
 	sudo cp ${PROJECT_ROOT}/buffpy/scripts/buffbot.service /etc/systemd/system
--- buffpy/scripts/install_ros.bash
+++ buffpy/scripts/install_ros.bash
@@ -3,7 +3,7 @@
 ROS_PKG=$2
 ROS_DISTRO=$1 
 
-echo -e "\n\tSetting up ROS ${ROS_DISTRO}\n"
+echo -e "\n\tSetting up ROS ${ROS_DISTRO} ${ROS_DISTRO}\n"
 
 # ROS installation
 # add repositories
@@ -19,11 +19,13 @@ sudo apt update
 #
 # Install ROS
 #
+echo -e "\n\tapt Installing ros-${ROS_DISTRO}-${ROS_PKG}\n"
 sudo apt install -y ros-${ROS_DISTRO}-${ROS_PKG}
 
 #
 # Install ROS dependencies
 #
+echo -e "\n\tpip Installing from ${PROJECT_ROOT}/buffpy/config/install/ros_dependencies.txt\n"
 python3 -m pip install -r ${PROJECT_ROOT}/buffpy/config/install/ros_dependencies.txt
 
 
@@ -34,7 +36,6 @@ echo -e "\n\tFinishing ROS setup...\n"
 #
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt update
-sudo apt install python-rosdep
 sudo rosdep init
 rosdep update
 
@@ -42,8 +43,9 @@ rosdep update
 # Install cv2 bridge for python3
 #
 sudo rm -rf /var/lib/apt/lists/*
-sudo apt update 
-cd /home/cu-robotics 
+sudo apt update
+cd ${PROJECT_ROOT}/..
+echo -e "\n\tgit cloning vision_opencv to ${pwd}/opencv_ws\n"
 mkdir opencv_ws 
 cd opencv_ws
 git clone https://github.com/ros-perception/vision_opencv.git src/vision_opencv
@@ -55,8 +57,9 @@ catkin init
 catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/$(uname -m)-linux-gnu/libpython3.6m.so --install --extend /opt/ros/melodic
 catkin build cv_bridge
 cp -r install/lib/python3/dist-packages/* /usr/lib/python3/dist-packages/
-cd /home/cu-robotics 
+cd $HOME
 rm -rf opencv_ws
+cd $PROJECT_ROOT
 
 
 
