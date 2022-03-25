d3adbb88e41bbcbdf2d3bf2d6cb431c904d8ef34
yatsenkoa <yatsenkoa2020@gmail.com>
2022-03-25 17:37:33 -0600

    working sine demo + add config file

--- src/hunter/scripts/tracker.py
+++ src/hunter/scripts/tracker.py
@@ -6,6 +6,14 @@ import numpy as np
 import buffvision as bv
 from std_msgs.msg import Float64MultiArray
 
+"""
+    Tracker class
+    args:
+    - configData: dictionary containing the configuration data
+        - data: do you want to save telemetry to a file under data
+    - Indicate that you want to save the predict
+"""
+
 
 class Tracker:
     def __init__(self, configData):
@@ -13,10 +21,13 @@ class Tracker:
         self.plot_count = 0
         self.DEBUG_SAVE_PATH = None
 
-        self.t = 1/30
+        self.dt = 1/30
+        self.t = 0
         self.state = []
         self.error = 0
         self.pred_prev = np.array((0, 0))
+
+        self.debug = True
 
         # previous groundtruth values. (3, 2 timesteps ago, 1 ago, now) We use these to update our deltas
         self.prev_1 = None
@@ -25,9 +36,9 @@ class Tracker:
         self.prev_4 = None
 
         # our predictions for 1 step ahead, 2, 3
-        self.pred_1 = None
-        self.pred_2 = None
-        self.pred_3 = None
+        self.pred_1 = (0, 0)
+        self.pred_2 = (0, 0)
+        self.pred_3 = (0, 0)
 
         # our deltas that we use to update predictions
         self.d_1 = None
@@ -37,7 +48,7 @@ class Tracker:
         self.debug = rospy.get_param('/buffbot/DEBUG')
         topics = rospy.get_param('/buffbot/TOPICS')
 
-        rospy.logerr(topics['detected_object'])
+        rospy.logerr(topics)
 
         self.topics = [topics[t] for t in configData['TOPICS']]
 
@@ -63,8 +74,13 @@ class Tracker:
         self.save_state()
 
         # send out predictions
-        predictions = [self.pred_1, self.pred_2, self.pred_3]
-        self.prediction_pub.publish(predictions)
+        predictions_arr = Float64MultiArray()
+        predictions = np.array((*self.pred_1, *self.pred_2, *self.pred_3))
+        predictions_arr.data = predictions
+        if(self.debug):
+            rospy.loginfo(
+                'current pos: {self.prev_1} pred_1: {self.pred_1} pred_2: {self.pred_2} pred_3: {self.pred_3} error: {self.error} d_1: {self.d_1}'.format(self=self))
+        self.prediction_pub.publish(predictions_arr)
 
     def mse(self, x, y):
         return ((x - y) ** 2).sum()
@@ -87,23 +103,24 @@ class Tracker:
 
             if self.prev_3 is not None:
                 vel_1 = self.d_1
-                vel_2 = (self.prev_2 - self.prev_3) / self.dt
+                vel_2 = (self.prev_2 - self.prev_3)
 
-                self.d_2 = (vel_1 - vel_2) / self.dt
+                self.d_2 = (vel_1 - vel_2)
 
                 if self.prev_4 is not None:
                     acc_1 = self.d_2
-                    vel_3 = (self.prev_3 - self.prev_4) / self.dt
-                    acc_2 = (vel_2 - vel_3) / self.dt
-                    acc_2 = (acc_1 - acc_2) / self.dt
+                    vel_3 = (self.prev_3 - self.prev_4)
+                    acc_2 = (vel_2 - vel_3)
+                    acc_2 = (acc_1 - acc_2)
 
-                    self.d_3 = (acc_1 - acc_2) / self.dt
+                    self.d_3 = (acc_1 - acc_2)
 
     def save_state(self):
         self.state.append((self.pred_prev, self.prev_1, self.d_1,
                           self.d_2, self.d_3, self.error, self.t))
 
     def update_predictions(self):
+
         if self.pred_1 is not None:
             if self.pred_2 is None:
                 self.pred_pred = np.array((0, 0))
@@ -112,14 +129,14 @@ class Tracker:
 
         if(self.d_1 is not None and self.d_2 is not None and self.d_3 is not None):
             self.pred_1 = self.prev_1 + \
-                (self.d_1 * self.dt) + (self.d_2 * (self.dt**2) / 2) + \
-                (self.d_3 * self.dt**3) / 6
+                (self.d_1 * self.dt) + (self.d_2 * (self.dt ** 2) / 2) + \
+                (self.d_3 * (self.dt ** 3)) / 6
             self.pred_2 = self.pred_1 + \
-                (self.d_1 * self.dt) + (self.d_2 * (self.dt**2) / 2) + \
-                (self.d_3 * self.dt**3) / 6
+                (self.d_1 * self.dt) + (self.d_2 * (self.dt ** 2) / 2) + \
+                (self.d_3 * (self.dt ** 3)) / 6
             self.pred_3 = self.pred_2 + \
-                (self.d_1 * self.dt) + (self.d_2 * (self.dt**2) / 2) + \
-                (self.d_3 * self.dt**3) / 6
+                (self.d_1 * self.dt) + (self.d_2 * (self.dt ** 2) / 2) + \
+                (self.d_3 * (self.dt ** 3)) / 6
 
         else:
             self.pred_1 = self.prev_1
@@ -194,45 +211,3 @@ if __name__ == '__main__':
         with open(os.path.join(os.getenv('PROJECT_ROOT'), 'buffpy', 'config', 'data', sys.argv[1]), 'r') as f:
             data = yaml.safe_load(f)
         main(data)
-
-
-"""
-def display(self, samples):
-    sampleX, sampleY, vXActual, vYActual, aXActual, aYActual, sample_t = list(
-        zip(*samples))
-    pose, vel, accel, jerk, error, t = list(zip(*self.deltas))
-    poseX, poseY = list(zip(*pose))
-
-    vX, vY = list(zip(*vel))
-    aX, aY = list(zip(*accel))
-    jX, jY = list(zip(*jerk))
-    eX, eY = list(zip(*error))
-    fig, ax = plt.subplots(2,3, figsize=(20,20))
-    ax[0][0].legend(loc='upper right')
-    ax[0][0].set_title('Sampled vs Predicted x / y')
-    ax[0][1].plot(t, aX, label='Predicted X Accel', marker='x')
-    ax[0][1].plot(t, aY, label='Predicted Y Accel', marker='x')
-    ax[0][1].plot(sample_t, aXActual, label='Sample X Accel', marker='o')
-    ax[0][1].plot(sample_t, aYActual, label='Sample Y Accel', marker='o')
-    ax[0][1].legend(loc='upper right')
-    ax[0][1].set_title('Sampled vs Predicted Acceleration / dt')
-    ax[0][2].plot(t, vX, label='Predicted X Velocity', marker='x')
-    ax[0][2].plot(t, vY, label='Predicted Y Velocity', marker='x')
-    ax[0][2].plot(sample_t, vXActual,
-                    label='Sample X Velocity', marker='o')
-    ax[0][2].plot(sample_t, vYActual,
-                    label='Sample Y Velocity', marker='o')
-    ax[0][2].legend(loc='upper right')
-    ax[0][2].set_title('Sampled vs Predicted Velocity / dt')
-    ax[1][0].plot(t, eX, label='X Error', marker='x')
-    ax[1][0].plot(t, eY, label='Y Error', marker='x')
-    ax[1][0].legend(loc='upper right')
-    ax[1][0].set_title('Error over Time')
-    ax[1][1].set_title('Sampled vs Predicted x / t')
-    ax[1][1].plot(sample_t, sampleX, label='Sample X Pose', marker='o')
-    ax[1][1].plot(t, poseX, label='Predicted X Pose', marker='o')
-    ax[1][2].set_title('Sampled vs Predicted y / t')
-    ax[1][2].plot(sample_t, sampleY, label='Sample Y Pose', marker='o')
-    ax[1][2].plot(t, poseY, label='Predicted Y Pose', marker='o')
-    plt.show()
-"""
