d5dae582506c5bab0e7612b22843a347fbead309
Artem <artem@pop-os.localdomain>
2022-04-02 19:24:18 -0600

    add changes from friday

--- src/hunter/scripts/deadreckon_tracer.py
+++ src/hunter/scripts/deadreckon_tracer.py
@@ -33,9 +33,10 @@ class Dead_Reckon_Tracer:
 
 		self.FOV = rospy.get_param('/buffbot/CAMERA/FOV')
 
-		if 'M' in config_data and 'B' in config_data and 'DSCALE' in config_data:
+		if 'M' in config_data and 'B' in config_data and 'A' in config_data and 'DSCALE' in config_data:
 			self.m = config_data['M']
 			self.b = config_data['B']
+			self.a = config_data['A']
 			self.d_scale = config_data['DSCALE']
 		else:
 			rospy.logerr(f'DeadReckon tracker missing config elements: exiting...')
@@ -100,11 +101,11 @@ class Dead_Reckon_Tracer:
 				for (x,y) in self.history:
 					if x >= 0 and y >=0:
 						target = (int(origin[0] + x), int(origin[1] + y))
-						image = cv2.circle(image, target, 10, (0,255,0), 2)
+						image = cv2.circle(image, target, 5, (0,255,0), 2)
 
 				x,y = self.pose
-				pose = (int(origin[0] + y), int(origin[1] + x))
-				image = cv2.circle(image, pose, 10, (0,0,255))
+				pose = (int(origin[0] + x), int(origin[1] + y))
+				image = cv2.circle(image, pose, 5, (0,0,255))
 
 				msg = self.bridge.cv2_to_imgmsg(image, encoding='rgb8')
 				self.debug_pubs[topic].publish(msg)
@@ -139,13 +140,13 @@ class Dead_Reckon_Tracer:
 		"""
 		Projects a detection into the body frame
 		PARAMS:
-			pose: Float64MultiArray.data, [x,y,w,h,cf,cl] (detection msg)
-
+			pose: Float64MultiArray.data, [x,y,h,w,cf,cl] (detection msg)
 		RETURNS:
 			vector (x,y): body frame position of the detection
 		"""
-		d = (self.m / pose[3]) + self.b
-		alpha = np.radians((pose[0] / self.image_size[0]) * self.FOV / 2)
+		d = self.a * np.exp(self.m * (pose[2] + self.b))
+		rospy.loginfo(f'{d} {pose[2]} {pose[3]}')
+		alpha = np.radians((pose[0] / self.image_size[0]) * self.FOV) / 2
 		return d * np.array([np.cos(self.psi + alpha), np.sin(self.psi + alpha)])
 
 	def predict(self):
