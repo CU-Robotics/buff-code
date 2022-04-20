9ccd87fbdf16b79618caf959c612e83dbbe333db
Tyler-Scripps <tylerscripps@gmail.com>
2022-04-19 18:16:32 -0600

    minor change

--- src/wrecker/src/robot/robot.ino
+++ src/wrecker/src/robot/robot.ino
@@ -9,6 +9,8 @@
 #include "gimbal.h"
 
 unsigned long deltaT = 0; //variable to store time between loops
+unsigned long lastTime = 0;
+
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> chassisCAN;
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> SuperStructureCAN;
@@ -38,6 +40,8 @@ void setup() {
 }
 
 void loop() {
+  deltaT = micros() - lastTime;
+  lastTime = micros();
   gimbal.update(deltaT);
   
   
