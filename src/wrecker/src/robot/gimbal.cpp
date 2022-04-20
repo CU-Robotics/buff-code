ea8345beb5a824d23af5e2e9c826f3ccaa5b161a
Tyler-Scripps <tylerscripps@gmail.com>
2022-04-19 18:16:20 -0600

    COMPILES and started timing stuff

--- src/wrecker/src/robot/gimbal.cpp
+++ src/wrecker/src/robot/gimbal.cpp
@@ -10,12 +10,19 @@ gimbal::gimbal(RobotConfig *tempConfig, RobotInput *tempInput) {
 }
 
 void gimbal::init(CAN_message_t* sendMsg) {
+    pitchController.init(config->pitchP, config->pitchI, config->pitchD);
+    yawController.init(config->yawP, config->yawI, config->yawD);
+
     // this->pitchMotor = new gm6020();
     pitchMotor.init(config->pitchID, sendMsg);
     yawMotor.init(config->yawID, sendMsg);
 }
 
 void gimbal::update(float deltaTime) {
+    float newPitchPower = pitchController.calculate(pitchMotor.getAngle(), input->gimbalPitch, deltaTime);
+    float newYawPower = pitchController.calculate(yawMotor.getAngle(), input->gimbalPitch, deltaTime);
 
+    pitchMotor.setPower(newPitchPower);
+    yawMotor.setPower(newYawPower);
 }
 
