58dfa952aa1fbecff511f3da4f805aaecedd4f18
Isaac Wilcove <isaac@wilcove.com>
2022-04-30 19:50:46 -0600

    platform.io compatability

---
+++ src/wrecker/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#ifndef _FLEXCAN_T4_H_
+#include <FlexCAN_T4.h>
+#endif
+
+unsigned long deltaT = 0;
+unsigned long lastTime = 0;
+
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> chassisCAN;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> superStructureCAN;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  chassisCAN.begin();
+  superStructureCAN.begin();
+  chassisCAN.setBaudRate(1000000);
+  superStructureCAN.setBaudRate(1000000);
+}
+
+void loop() {
+
+
+  // delta-time calculator: keep this at the bottom
+  deltaT = micros() - lastTime;
+  lastTime = micros();
+}
---
+++ src/wrecker/src/state/state.h
@@ -0,0 +1,55 @@
+#define STATE_H
+
+struct S_SwerveChassis {
+  float heading;
+  float rpm;
+  float alpha; // angular acceleration
+  float xAccel;
+  float yAccel;
+};
+
+struct S_RailChassis {
+  float position;
+  float vel;
+  float accel;
+};
+
+struct S_Gimbal {
+  float pitch;
+  float yaw;
+  float yawGlobal;
+};
+
+struct S_17mmShooter {
+  bool firing;
+};
+
+struct S_42mmShooter {
+  bool firing;
+};
+
+struct S_DriverInput {
+  float mouseX;
+  float mouseY;
+
+  bool w;
+  bool a;
+  bool s;
+  bool d;
+  bool q;
+  bool e;
+  bool r;
+  bool f;
+  bool g;
+  bool z;
+  bool x;
+  bool c;
+  bool space;
+  bool shift;
+  bool ctrl;
+};
+
+struct S_RefSystem {
+  short robotLevel;
+  int matchTimer;
+};
