42a175667a4a4ef34d7a495d231ba0df593ade63
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-05-14 20:54:54 -0600

    initial flywheel files

---
+++ src/wrecker/src/drivers/flywheel.h
@@ -0,0 +1,13 @@
+#ifndef FLYWHEEL_H
+#define FLYWHEEL_H
+#include <Arduino.h>
+
+class flywheel {
+    private:
+        byte pinNum;
+    public:
+        flywheel();
+        void init(byte tempPinNum);
+};
+
+#endif
