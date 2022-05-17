70edf18174ad8c0032fb7676c929ac24117a9f02
Tyler-Scripps <tylerscripps@gmail.com>
2022-05-16 19:58:00 -0600

    setpower function completed

--- src/wrecker/src/drivers/flywheel.cpp
+++ src/wrecker/src/drivers/flywheel.cpp
@@ -11,5 +11,13 @@ void flywheel::init(byte tempPinNum) {
 }
 
 void setPower(float newPower) {
-    
+    if (newPower > 1)
+    {
+        newPower = 1;
+    } else if (newPower < 0) {
+        newPower = 0;
+    }
+    int sendPower = 0;
+    sendPower = map(newPower * 1000, 0, 1000, 0, 255);
+    analogWrite(pinNum, sendPower);
 }
