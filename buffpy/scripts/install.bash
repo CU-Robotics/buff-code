a19711f6304235230a1abf3a78e1ce13d990418d
Mitchell D Scott <misc4432@colorado.edu>
2022-06-03 11:56:09 -0600

    dah

--- buffpy/scripts/install.bash
+++ buffpy/scripts/install.bash
@@ -69,35 +69,9 @@ sudo apt update
 #	Install Utilities
 #
 
-#	Setup Teensy Rules
-curl https://www.pjrc.com/teensy/00-teensy.rules -O
-
-# mv rules into rules.d and set the proper file permissions
-sudo mv 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
-sudo chmod 0644 /etc/udev/rules.d/00-teensy.rules
+source "${PROJECT_ROOT}/buffpy/scripts/install_tytools.bash"
 
 if [[ "${HOSTNAME}" == "edge"* ]]; then
-	echo -e "\n\tInstalling TYcmd...\n"
-
-	cd "${PROJECT_ROOT}/.."
-
-	sudo apt-get install build-essential cmake libudev-dev qtbase5-dev pkg-config
-
-	wget https://github.com/Koromix/tytools/archive/refs/tags/v0.9.7.tar.gz
-
-	tar -xf v0.9.7.tar.gz
-
-	cd tytools-0.9.7
-	
-	mkdir -p build/linux && cd build/linux
-	cmake -DCMAKE_INSTALL_PREFIX=/usr/local ../..
-	make
-
-	sudo make install
-
-	cd "${PROJECT_ROOT}"
-
-	rm -rf ../tytools-0.9.7
 
 	sudo cp ${PROJECT_ROOT}/buffpy/scripts/buffbot.service /etc/systemd/system
 
