23fbd3d57f685855903587cae32936f3d3dac89c
Isaac Wilcove <isaac@wilcove.com>
2022-06-17 00:09:57 -0600

    sentry updates

--- src/wrecker/src/subsystems/gimbal.cpp
+++ src/wrecker/src/subsystems/gimbal.cpp
@@ -114,8 +114,12 @@ void Gimbal::update(float deltaTime) {
 
   // Set motor power
   if (calibrated) {
-    yawMotor.setPower(state->gimbal.yawVel.Y + dynamicYawFeedforward);
-    pitchMotor.setPower(state->gimbal.pitchVel.Y + dynamicPitchFeedForward);
+    // yawMotor.setPower(state->gimbal.yawVel.Y + dynamicYawFeedforward);
+    // pitchMotor.setPower(state->gimbal.pitchVel.Y + dynamicPitchFeedForward);
+    yawMotor.setPower(0.0);
+    pitchMotor.setPower(0.0);
+
+    Serial.print(yawMotor.getAngle());
   }
 }
 
