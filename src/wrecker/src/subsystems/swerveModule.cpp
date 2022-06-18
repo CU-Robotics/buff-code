772cf4f79103bfd7f650b99eac06ea6019ac1df0
Isaac Wilcove <isaac@wilcove.com>
2022-06-18 12:51:55 -0600

    wat

--- src/wrecker/src/subsystems/swerveModule.cpp
+++ src/wrecker/src/subsystems/swerveModule.cpp
@@ -101,10 +101,6 @@ void SwerveModule::update(float speed, float angle, float deltaTime) {
 
   // Set motor power
   if (calibrated) {
-    // steerMotor.setPower(0.0);
-    // Serial.print(steerMotor.getAngle());
-    // Serial.print(" - ");
-
     steerMotor.setPower(tmp_steerVel.Y);
 
     // Only drive if sufficiently close to target angle
