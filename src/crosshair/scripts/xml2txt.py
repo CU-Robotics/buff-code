763087b278a8503e8a9f913a054a06921de3993f
Mitchell Scott <misc4432@colorado.edu>
2022-06-18 23:09:28 -0600

    lsjl

--- src/crosshair/scripts/xml2txt.py
+++ src/crosshair/scripts/xml2txt.py
@@ -7,7 +7,7 @@ import numpy as np
 import buffvision as bv
 import xml.etree.ElementTree as ET
 
-def get_txt_from_xml(label):
+def get_txt_from_xml(label, image_shape):
 	labels = []
 
 	objects = label.findall('object')
@@ -19,11 +19,17 @@ def get_txt_from_xml(label):
 				if color.text == 'blue':
 					cl = 0
 
-				else:
+				elif color.text == 'red':
 					cl = 1
 
+				elif color.text == 'purple':
+					cl = 2
+
+				elif color.text == 'grey':
+					continue
+
 		elif not name == 'base':
-			cl = 2
+			cl = 3
 
 		else:
 			continue
@@ -33,13 +39,107 @@ def get_txt_from_xml(label):
 		ymin = round(float(bound.find('ymin').text))
 		xmax = round(float(bound.find('xmax').text))
 		ymax = round(float(bound.find('ymax').text))
-		xcenter = (xmax + xmin) / 2
-		ycenter = (ymax + ymin) / 2
-		width = abs(xmax - xmin)
-		hieght = abs(ymax - ymin)
+		xcenter = (xmax + xmin) / (2 * image_shape[1])
+		ycenter = (ymax + ymin) / (2 * image_shape[0])
+		width = abs(xmax - xmin) / image_shape[1]
+		hieght = abs(ymax - ymin) / image_shape[0]
+
 		labels.append([cl, xcenter, ycenter, width, hieght])
 
 	return labels
+
+def fix_collision(x, y, w, h, labels):
+
+	adjusted = False
+	x1,y1,x2,y2 = bv.xywh2xyxy(x, y, w, h)
+
+
+	for [c, xl, yl, wl, hl] in labels:
+		xa,ya,xb,yb = bv.xywh2xyxy(xl, yl, wl, hl)
+
+		if (y1 > ya and y1 < yb) or (y2 > ya and y2 < yb):
+			if x1 > xa and x1 < xb:
+				adjusted = True
+				if x1 > (xa + xb) / 2:
+					x1 = xa
+				else:
+					x1 = xb
+
+			if x2 > xa and x2 < xb:
+				adjusted = True
+				if x2 > (xa + xb) / 2:
+					x2 = xa
+				else:
+					x2 = xb
+
+		if (x1 > xa and x1 < xb) or (x2 > xa and x2 < xb):
+			if y1 > ya and y1 < yb:
+				adjusted = True
+				if y1 > (ya + yb) / 2:
+					y1 = ya
+				else:
+					y1 = yb
+
+			if y2 > ya and y2 < yb:
+				adjusted = True
+				if y2 > (ya + yb) / 2:
+					y2 = ya
+				else:
+					y2 = yb
+
+	return (x1 + x2) / 2, (y1 + y2) / 2, abs(x1 - x2), abs(y1 - y2), adjusted
+
+
+def fit_crop(image, x, y, w, h, labels):
+
+	max_i = 50
+	for j in range(max_i):
+		print(f'Fitting {j}/{max_i}')
+		x, y, w, h, adj = fix_collision(x, y, w, h, labels)
+
+		if not adj:
+			return x, y, w, h
+
+	return -1, -1, -1, -1
+
+def filter_data(image, orig_labels):
+
+	n_samples = 1
+	img_shape = [416, 416]
+
+	img_shape[0] = min(image.shape[0], img_shape[0])
+	img_shape[1] = min(image.shape[1], img_shape[1])
+
+	for i in range(n_samples):
+		# samples between img_shape / 2 and image.shape - img_shape / 2
+		w = img_shape[1] / image.shape[1]
+		h = img_shape[0] / image.shape[0]
+		x = np.random.uniform(w / 2, 1 - (w / 2))
+		y = np.random.uniform(h / 2, 1 - (h / 2))
+
+		x, y, w, h = fit_crop(image, x, y, w, h, orig_labels)
+
+		if x < 0 or y < 0:
+			return None, None
+
+		labels = []
+		# print(f'X range {x - w2}, {x + w2}')
+		# print(f'Y range {y - h2}, {y + h2}')
+		# print(f'{w2} {h2}')
+		for c, xl, yl, wl, hl in orig_labels:
+			if abs(xl - x) < w / 2:
+				if abs(yl - y) < h / 2:
+					#x1, y1, x2, y2 = bv.xywh2xyxy(xl,yl,wl,hl)
+					print([c, round((xl - x + (w / 2)) * image.shape[1]), round((yl - y + (h / 2)) * image.shape[0]), wl * image.shape[1], hl * image.shape[0]])
+					label = np.array([c, round((xl - x + (w / 2)) * image.shape[1]) / img_shape[1], round((yl - y + (h / 2)) * image.shape[0]) / img_shape[0], round(wl * image.shape[1]) / img_shape[1], round(hl * image.shape[0]) / img_shape[0]])
+					labels.append(label)
+
+		cropped = bv.crop_image(image.copy(), x, y, w, h)
+		print(cropped.shape)
+		print(img_shape)
+		bv.display_annotated(cropped, labels)
+
+	return None, None
 
 
 def main(data_dir):
@@ -66,8 +166,10 @@ def main(data_dir):
 		jpg_filename = os.path.join(data_path, 'image', fname + '.jpg')
 		if os.path.exists(jpg_filename):
 			image = cv2.imread(jpg_filename)
-			label = get_txt_from_xml(xml)
-			bv.save_txt_label_data([image], [label], gen_path)
+			labels = get_txt_from_xml(xml, image.shape)
+			images, labels = filter_data(image, labels)
+			continue
+			bv.save_txt_label_data(images, labels, gen_path)
 			sample_counter += 1
 		else:
 			print(f'No image file {jpg_filename}')
