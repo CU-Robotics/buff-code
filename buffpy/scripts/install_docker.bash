facd37e9441be0044556689e044032dd6d929810
cu-boulder-robotics <114837046+cu-boulder-robotics@users.noreply.github.com>
2022-10-02 14:11:38 -0600

    Update install_docker.bash

--- buffpy/scripts/install_docker.bash
+++ buffpy/scripts/install_docker.bash
@@ -2,9 +2,9 @@
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install \
+sudo apt -y install \
 	ca-certificates \
 	curl \
 	gnupg \
@@ -15,15 +15,15 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
 sudo apt update
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 docker run hello-world
 
-sudo apt-get install qemu binfmt-support qemu-user-static
+sudo apt -y install qemu binfmt-support qemu-user-static
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
