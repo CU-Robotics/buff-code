0b5478af11d9ada0c85f5d176f6af35ebef31fca
Matt Harper <matthew.harper@colorado.edu>
2023-01-21 21:07:55 -0700

    HidReader docs

--- src/buff_rust/src/teensy_comms/buff_hid.rs
+++ src/buff_rust/src/teensy_comms/buff_hid.rs
@@ -115,6 +115,8 @@ impl HidWriter {
     }
 }
 
+/// Responsible for initializing [BuffBotStatusReport] and continuously
+/// sending status reports
 pub struct HidReader {
     pub shutdown: Arc<RwLock<bool>>,
     pub input: ByteBuffer,
@@ -134,11 +136,18 @@ impl HidReader {
         }
     }
 
-    /// read the bytes from the input buffer
-    /// Usage:
-    /// '''
-    ///     hidreader.read(idx, bytes); // bytes will be sent next write
-    /// '''
+    /// Read data into the input buffer and return how many bytes were read
+    ///
+    /// # Usage
+    ///
+    /// ```
+    /// match reader.read() {
+    ///     64 => {
+    ///         // packet OK, do something
+    ///     }
+    ///     _ => {} // do nothing
+    /// }
+    /// ```
     pub fn read(&mut self) -> usize {
         match &self.teensy.read(&mut self.input.data) {
             Ok(value) => {
@@ -153,12 +162,19 @@ impl HidReader {
     }
 
     /// After requesting a report this can be used to wait for a reply
-    /// Usage:
-    /// '''
-    ///     // set writer.output.data[0] to an int [1-3] (255 for initializer)
-    ///     writer.write();
-    ///     reader.wait_for_report_reply(255, 10);
-    /// '''
+    ///
+    /// # Panics
+    ///
+    /// This will panic if a reply is not received from the Teensy
+    /// within `timeout` ms.
+    ///
+    /// # Usage
+    ///
+    /// ```
+    /// // set writer.output.data[0] to an int [1-3] (255 for initializer)
+    /// writer.write();
+    /// reader.wait_for_report_reply(255, 10);
+    /// ```
     pub fn wait_for_report_reply(&mut self, packet_id: u8, timeout: u128) {
         let mut loopt;
         let t = Instant::now();
@@ -192,13 +208,15 @@ impl HidReader {
     }
 
     /// Parse the stored HID packet into BuffBot Data Structures
-    /// Usage:
-    /// '''
-    ///     // HID packet waiting
-    ///     if reader.read() > 0 { // read returns the number of bytes (64 or bust)
-    ///         reader.parse_report();
-    ///     }
-    /// '''
+    ///
+    /// # Usage
+    ///
+    /// ```
+    /// // HID packet waiting
+    /// if reader.read() > 0 { // read returns the number of bytes (64 or bust)
+    ///     reader.parse_report();
+    /// }
+    /// ```
     pub fn parse_report(&mut self) {
         if self.input.get_i32(60) > 1000 {
             println!("Teensy cycle time is over the limit");
@@ -243,11 +261,16 @@ impl HidReader {
 
     /// Main function to spin and connect the teensys
     /// input to ROS.
-    /// Usage:
-    /// '''
-    ///     let reader = HidReader::new();
-    ///     reader.spin();       // runs until watchdog times out
-    /// '''
+    ///
+    /// # Usage
+    /// ```
+    /// use hidapi::HidApi;
+    /// use buff_rust::teensy_comms::buff_hid::HidReader;
+    ///
+    /// let mut hidapi = HidApi::new().expect("Failed to create API instance");
+    /// let mut reader = HidReader::new(&mut hidapi, vid, pid);
+    /// reader.spin();       // runs until watchdog times out
+    /// ```
     pub fn spin(&mut self) {
         while !*self.shutdown.read().unwrap() {
             let loopt = Instant::now();
@@ -264,6 +287,12 @@ impl HidReader {
         }
     }
 
+    /// Sends robot status report packet to [HidROS], waits for the reply packet,
+    /// then calls [HidReader::spin] to begin parsing reports
+    ///
+    /// # Example
+    ///
+    /// see [HidLayer::pipeline()]
     pub fn pipeline(
         &mut self,
         shutdown: Arc<RwLock<bool>>,
