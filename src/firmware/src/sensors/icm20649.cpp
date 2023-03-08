9964a077c3adbed2288768c358814fd7f74e4647
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2023-03-07 19:25:18 -0700

    finishedISH icm20649 driver and test

--- src/firmware/src/sensors/icm20649.cpp
+++ src/firmware/src/sensors/icm20649.cpp
@@ -4,10 +4,28 @@ icm20649::icm20649() {
     // start i2c communications
     icm.begin_I2C();
     
-    // get the sensor objects linked ot the sensor
+    // get the sensor objects linked to the sensor
     icm_temp = icm.getTemperatureSensor();
     icm_accel = icm.getAcellerometerSensor();
     icm_gyro = icm.getGyroSensor();
-    
-    
+}
+
+void icm20649::readSensor() {
+    icm_temp = icm.getTemperatureSensor();
+    icm_accel = icm.getAcellerometerSensor();
+    icm_gyro = icm.getGyroSensor();
+}
+
+void icm20649::readSensor() {
+    icm_temp->getEvent(&temp);
+    icm_accel->getEvent(&accel);
+    icm_gyro->getEvent(&gyro);
+
+    accelX = accel.acceleration.x;
+    accelY = accel.acceleration.y;
+    accelZ = accel.acceleration.z;
+
+    gyroX = gyro.gyro.x;
+    gyroY = gyro.gyro.Y;
+    gyroZ = gyro.gyro.Z;
 }
