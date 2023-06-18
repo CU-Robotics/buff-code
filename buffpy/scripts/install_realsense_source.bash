f10b4cd12c5a2df05a66d7ba0f40f47de5dd4102
edgek3 <curobotics@colorado.edu>
2023-06-18 02:40:11 -0600

    Update install script to be more exact with realsense

--- buffpy/scripts/install_realsense_source.bash
+++ buffpy/scripts/install_realsense_source.bash
@@ -5,6 +5,6 @@ sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
 cd ${HOME} && \
 	git clone https://github.com/IntelRealSense/librealsense.git && \
-	cd librealsense && mkdir -p build && cd build && \
-	cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=false -DCMAKE_BUILD_TYPE=release && \
-	sudo make uninstall && make clean && make && sudo make install
+	cd librealsense && git checkout b874e42 && mkdir -p build && cd build && \
+	cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=false -DCMAKE_BUILD_TYPE=release -DPYTHON_EXECUTABLE=`which python3.8` && \
+	sudo make uninstall && make clean && make -j4 && sudo make install
