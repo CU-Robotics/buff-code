59683a9b42aafa68207ed703adc0f3491f31cdd6
Edge K4 <curobotics@colorado.edu>
2023-06-18 22:17:36 -0600

    WORKING install script

--- buffpy/scripts/install_realsense_source.bash
+++ buffpy/scripts/install_realsense_source.bash
@@ -1,10 +1,8 @@
-sudo pip install pybind11
-sudo pip install pyrealsense2
-
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
-cd ${HOME} && \
-	git clone https://github.com/IntelRealSense/librealsense.git && \
-	cd librealsense && git checkout b874e42 && mkdir -p build && cd build && \
-	cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=false -DCMAKE_BUILD_TYPE=release -DPYTHON_EXECUTABLE=`which python3.8` && \
+cd ${HOME} && rm -rf librealsense && \
+	git checkout https://github.com/IntelRealSense/librealsense.git && git checkout 23b0904 && cd librealsense && mkdir -p build && cd build && \
+	cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=true -DCMAKE_BUILD_TYPE=debug -DPYTHON_EXECUTABLE=`which python3.8` && \
 	sudo make uninstall && make clean && make -j4 && sudo make install
+	
+echo 'export PYTHONPATH="$PYTHONPATH:/usr/lib/python3.8/site-packages/pyrealsense2"' >> ~/.bashrc
