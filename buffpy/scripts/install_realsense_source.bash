de768097be406ab377e8cecf36732bfd67ec91a7
edgek2 <curobotics@colorado.edu>
2023-06-21 09:11:52 +0800

    iunstall script is better

--- buffpy/scripts/install_realsense_source.bash
+++ buffpy/scripts/install_realsense_source.bash
@@ -1,8 +1,8 @@
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
 cd ${HOME} && rm -rf librealsense && \
-	git checkout https://github.com/IntelRealSense/librealsense.git && git checkout 23b0904 && cd librealsense && mkdir -p build && cd build && \
+	git clone https://github.com/IntelRealSense/librealsense.git && cd librealsense && git checkout 23b0904 && mkdir -p build && cd build && \
 	cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=true -DCMAKE_BUILD_TYPE=debug -DPYTHON_EXECUTABLE=`which python3.8` && \
 	sudo make uninstall && make clean && make -j4 && sudo make install
-	
+
 echo 'export PYTHONPATH="$PYTHONPATH:/usr/lib/python3.8/site-packages/pyrealsense2"' >> ~/.bashrc
