4f5709eda6806541c83c8e94f0aff3d551c6c89b
Isaac Wilcove <isaac@wilcove.com>
2023-07-01 20:43:31 -0600

    Test 3

--- src/firmware/src/buff_cpp/device_manager.cpp
+++ src/firmware/src/buff_cpp/device_manager.cpp
@@ -413,7 +413,7 @@ void Device_Manager::push_can(){
 	Author: Mitchell Scott
 */
 void Device_Manager::read_sensors() {
-	if (micros() - prev_ref_read_micros > 100) {
+	if (micros() - prev_ref_read_micros > 1) {
 		prev_ref_read_micros = micros();
 		ref.read_serial();
 		controller_manager.team_color = ref.data.team_color;
